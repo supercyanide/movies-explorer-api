b3855f21ed293fbfb2b7cc16c43261fb99df4488
Lilya Schedrina <13cutepie@gmail.com>
2023-07-25 15:50:17 +0400

    Update users.js

--- controllers/users.js
+++ controllers/users.js
@@ -80,6 +80,7 @@ const updateProfile = (req, res, next) => {
 
 const login = (req, res, next) => {
   const { email, password } = req.body;
+  console.log(email, password);
   User.findUserByCredentials(email, password)
     .then((user) => {
       const token = jwt.sign({ _id: user._id }, NODE_ENV === 'production' ? JWT_SECRET : 'some-key', { expiresIn: '7d' });
